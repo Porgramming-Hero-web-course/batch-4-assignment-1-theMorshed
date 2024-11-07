012712e1c5fabbc13b6d3bf7ebec3369bb1ff9d0
theMorshed <themorshedctg@gmail.com>
2024-11-07 09:20:30 +0600

    problem 4 partial update

--- problem-4.ts
+++ problem-4.ts
@@ -1,29 +1,41 @@
+// Circle type declared
 type Circle = {
-    shape: string;
+    shape: "circle";
     radius: number;
 }
+
+// Rectangle type declared
 type Rectangle = {
-    shape: string;
+    shape: "rectangle";
     width: number;
     height: number;
 }
-type CircleAndRectangle = Circle | Rectangle;
 
-function calculateShapeArea(userShape: CircleAndRectangle): void {
+// Circle | Rectangle type declared
+type CircleOrRectangle = Circle | Rectangle;
+
+// calculateShapeArea function declaration
+function calculateShapeArea(userShape: CircleOrRectangle): number {
+    let area: number = 0;
     if (userShape.shape === "circle") {
-        const area = Math.PI * userShape.radius * userShape.radius;
-        console.log(area.toFixed(2));
+        area = Math.PI * userShape.radius * userShape.radius;
     }
     else if (userShape.shape == "rectangle") {
-        const area = userShape.width * userShape.height;
-        console.log(area);
+        area = userShape.width * userShape.height;
     }
+    return area;
 }
 
+// invoke function 
 const circleArea = calculateShapeArea({ shape: "circle", radius: 5 });
 
+// invoke function
 const rectangleArea = calculateShapeArea({
   shape: "rectangle",
   width: 4,
   height: 6,
 });
+
+// print both area
+console.log(circleArea);
+console.log(rectangleArea);
