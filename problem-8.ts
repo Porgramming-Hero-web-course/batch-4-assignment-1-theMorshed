a4fdd812cd6e9272077f17cafff4eb662c7fd193
theMorshed <themorshedctg@gmail.com>
2024-11-07 09:51:32 +0600

    problem 8 update

--- problem-8.ts
+++ problem-8.ts
@@ -1,6 +1,17 @@
-function validateKeys<T>(obj: T, keys: (keyof T)[]): boolean {
+// Declare a interface for T object
+interface T {
+    name: string;
+    age: number;
+    email: string;
+}
+
+// validateKeys function declaration
+function validateKeys(obj: T, keys: (keyof T)[]): boolean {
     return keys.every(key => key in obj);
 }
 
+// create a person object
 const person = { name: "Alice", age: 25, email: "alice@example.com" };
 console.log(validateKeys(person, ["name", "age"]));
+
+
